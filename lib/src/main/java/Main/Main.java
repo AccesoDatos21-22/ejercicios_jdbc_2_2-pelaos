098392dcac732aac1ad2a852eafc48e1dd14e4c1
trissaik <trissaikyt.g@gmail.com>
2021-11-15 21:23:38 +0100

    ejercicio6.clean Reorganizado main

--- lib/src/main/java/Main/Main.java
+++ lib/src/main/java/Main/Main.java
@@ -11,30 +11,28 @@ public class Main {
 
 		try {
 			Libros libros = new Libros();
+
+			libros.anadirLibro(new Libro(1, "1001 noches", "alibaba", "planeta", 200, 1000000));
+			List lista = libros.verCatalogo();
+			lista.forEach(System.out::println);
+			System.out.println("Libro por editorial");
+			libros.librosporEditorial("planeta");
+
+			for (Libro verCatalogo : libros.verCatalogo()) {
+				System.out.println(verCatalogo.toString());
+			}
+
+//			System.out.println(libros.crearTablaLibro());
+
+            libros.obtenerLibro(1325);
+            for (String columna : libros.getCamposLibro()) {
+                System.out.println(columna);
+            }
+            libros.borrar(new Libro(12345,"Sistemas Operativos","Tanembaun","Informatica",156,3));
+
 			libros.actualizaPrecio(1, 233.2f, 34);
-//			libros.anadirLibro(new Libro(1, "1001 noches", "alibaba", "planeta", 200, 1000000));
-//			List lista = libros.verCatalogo();
-//			lista.forEach(System.out::println);
-//			System.out.println("Libro por editorial");
-//			libros.librosporEditorial("planeta");
-//
-//			for (Libro verCatalogo : libros.verCatalogo()) {
-//				System.out.println(verCatalogo.toString());
-//			}
-//
-////			System.out.println(libros.crearTablaLibro());
-//
-//            libros.obtenerLibro(1325);
-//            for (String columna : libros.getCamposLibro()) {
-//                System.out.println(columna);
-//            }
-//            libros.borrar(new Libro(12345,"Sistemas Operativos","Tanembaun","Informatica",156,3));
-//
-//
-//		} catch (AccesoDatosException | SQLException e) {
-//			e.printStackTrace();
-//		}
-		} catch (AccesoDatosException e) {
+
+		} catch (AccesoDatosException | SQLException e) {
 			e.printStackTrace();
 		}
 	}
