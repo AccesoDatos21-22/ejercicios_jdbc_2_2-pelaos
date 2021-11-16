9e11f6ba889c9080b8bd529c4ff2c132e6a78dda
trissaik <trissaikyt.g@gmail.com>
2021-11-16 19:33:36 +0100

    Master.Main changed Arreglado cambios del merge anterior

--- lib/src/main/java/Main/Main.java
+++ lib/src/main/java/Main/Main.java
@@ -3,32 +3,35 @@ package Main;
 import dao.Libros;
 import modelo.AccesoDatosException;
 import modelo.Libro;
+
 import java.util.List;
 import java.sql.SQLException;
 
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Libros libros = null;
-		try {
-			libros = new Libros();
-			libros.anadirLibro(new Libro(1, "1001 noches", "alibaba", "planeta", 200, 1000000));
-			libros.anadirLibro(new Libro(2, "2021 noches", "aliexpress", "luna", 400, 1000000));
-			libros.anadirLibro(new Libro(3, "10 dias", "amazon", "asteroide", 600, 1000000));
-			List lista = libros.verCatalogo();
-			lista.forEach(System.out::println);
-			System.out.println("//////////////////////////////VER CATALOGO INVERSO");
-			libros.verCatalogoInverso();
-		} catch (AccesoDatosException e) {
-			Libros libros = new Libros();
-			int[] filas={1,3};
-			for (Libro libro : libros.verCatalogo()) {
-				System.out.println(libro);
-			}
-			System.out.println("//////////VER CATALOGO POR LISTA");
-			libros.verCatalogo(filas);
-			} catch (AccesoDatosException e) {
-			e.printStackTrace();
-		}
-	}
+        Libros libros = null;
+        try {
+            libros = new Libros();
+            libros.anadirLibro(new Libro(1, "1001 noches", "alibaba", "planeta", 200, 1000000));
+            libros.anadirLibro(new Libro(2, "2021 noches", "aliexpress", "luna", 400, 1000000));
+            libros.anadirLibro(new Libro(3, "10 dias", "amazon", "asteroide", 600, 1000000));
+            List lista = libros.verCatalogo();
+            lista.forEach(System.out::println);
+
+            System.out.println("////////////VER CATALOGO INVERSO");
+            libros.verCatalogoInverso();
+            int[] filas = {1, 3};
+            for (Libro libro : libros.verCatalogo()) {
+                System.out.println(libro);
+            }
+            System.out.println("//////////VER CATALOGO POR LISTA");
+            libros.verCatalogo(filas);
+
+
+
+        } catch (AccesoDatosException e) {
+            e.printStackTrace();
+        }
+    }
 }
